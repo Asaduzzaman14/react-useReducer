39e2ad593171bce43085dc5884bdb51f0e58604c
Md Asaduzaman <asadasad9282@gmail.com>
2022-12-04 21:19:00 +0600

    from controll using react reducer

---
+++ src/page/loginForm.js
@@ -0,0 +1,266 @@
+import React, { useReducer } from "react";
+
+const LongForm = () => {
+
+    const initialState = {
+        firstName: '',
+        lastName: '',
+        email: '',
+        gender: '',
+        education: '',
+        quntity: 1,
+        feedback: '',
+        term: false
+    }
+
+    const reducer = (state, action) => {
+        // console.log(action);
+
+        switch (action.type) {
+            case "INPUT":
+                return {
+                    ...state, [action.paylode.name]: action.paylode.value
+                };
+            case "TOGGOL":
+                return {
+                    ...state,
+                    term: !state.term
+                };
+            case "INCREMENT":
+                return {
+                    ...state,
+                    quntity: Number(state.quntity) + 1
+                };
+            case "DECREMENT":
+                return {
+                    ...state,
+                    quntity: Number(state.quntity) + 1
+                }
+
+
+
+            default: return state
+        }
+
+    }
+
+    const [state, dispatch] = useReducer(reducer, initialState)
+
+    const submit = (e) => {
+        e.preventDefault()
+        console.log(state);
+    }
+
+    return (
+        <div className='h-screen w-screen flex justify-center items-center overflow-auto'>
+            <form
+                className='shadow-lg border  p-10 rounded-md flex flex-wrap gap-3 max-w-3xl justify-between'
+                onSubmit={submit}
+            >
+                <div className='flex flex-col w-full max-w-xs'>
+                    <label className='mb-2' htmlFor='firstName'>
+                        First Name
+                    </label>
+                    <input
+                        type='text'
+                        name='firstName'
+                        id='firstName'
+                        className="border border-black rounded-sm"
+                        onBlur={(event) => {
+                            dispatch({
+                                type: "INPUT",
+                                paylode: { name: event.target.name, value: event.target.value }
+                            })
+                        }}
+                    />
+                </div>
+
+                <div className='flex flex-col w-full max-w-xs'>
+                    <label className='mb-2' htmlFor='lastName'>
+                        Last Name
+                    </label>
+                    <input
+                        type='text'
+                        name='lastName'
+                        id='lastName'
+                        className="border border-black rounded-sm"
+                        onBlur={(event) => {
+                            dispatch({
+                                type: "INPUT",
+                                paylode: { name: event.target.name, value: event.target.value }
+                            })
+                        }}
+                    />
+                </div>
+                <div className='flex flex-col w-full max-w-xs'>
+                    <label className='mb-2' htmlFor='email'>
+                        Email
+                    </label>
+                    <input
+                        type='email'
+                        name='email'
+                        id='email'
+                        className="border border-black rounded-sm"
+                        onBlur={(event) => {
+                            dispatch({
+                                type: "INPUT",
+                                paylode: { name: event.target.name, value: event.target.value }
+                            })
+                        }}
+                    />
+                </div>
+                <div className='flex flex-col w-full max-w-xs'>
+                    <h1 className='mb-3'>Gender</h1>
+                    <div className='flex gap-3'>
+                        <div>
+                            <input
+                                type='radio'
+                                id='male'
+                                name='gender'
+                                value='male'
+                                onClick={(event) => {
+                                    dispatch({
+                                        type: "INPUT",
+                                        paylode: { name: event.target.name, value: event.target.value }
+                                    })
+                                }}
+                            />
+                            <label className='ml-2 text-lg' htmlFor='male'>
+                                Male
+                            </label>
+                        </div>
+                        <div>
+                            <input
+                                type='radio'
+                                id='female'
+                                name='gender'
+                                value='female'
+                                onClick={(event) => {
+                                    dispatch({
+                                        type: "INPUT",
+                                        paylode: { name: event.target.name, value: event.target.value }
+                                    })
+                                }}
+                            />
+                            <label className='ml-2 text-lg' htmlFor='female'>
+                                Female
+                            </label>
+                        </div>
+                        <div>
+                            <input
+                                type='radio'
+                                id='other'
+                                name='gender'
+                                value='other'
+                                onClick={(event) => {
+                                    dispatch({
+                                        type: "INPUT",
+                                        paylode: { name: event.target.name, value: event.target.value }
+                                    })
+                                }}
+                            />
+                            <label className='ml-2 text-lg' htmlFor='other'>
+                                Other
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <div className='flex flex-col w-full max-w-xs'>
+                    <label className='mb-3' htmlFor='education'>
+                        Education
+                    </label>
+                    <select
+                        name='education'
+                        id='education'
+                        className="border border-black rounded-sm"
+                        onChange={(event) => {
+                            dispatch({
+                                type: "INPUT",
+                                paylode: { name: event.target.name, value: event.target.value }
+                            })
+                        }}
+                    >
+                        <option value='SSC'>SSC</option>
+                        <option value='HSC'>HSC</option>
+                        <option value='underGrad'>Under Graduate</option>
+                        <option value='graduate'>Graduate</option>
+                    </select>
+                </div>
+
+                <div className='flex flex-col w-full max-w-xs'>
+                    <label className='mb-3'>Number of PCs</label>
+                    <div className='flex justify-between items-center gap-2 '>
+                        <button
+                            className='bg-indigo-500 text-lg text-white rounded h-10 w-10 '
+                            onClick={(event) => {
+                                dispatch({
+                                    type: "DECREMENT",
+                                })
+                            }}
+                        >
+                            -
+                        </button>
+                        <div className='border flex-1 flex justify-center items-center h-10 rounded-md border-gray-300'>
+                            <span className='text-lg'>{state.quntity}</span>
+                        </div>
+                        <button
+                            className='bg-indigo-500 text-lg text-white rounded h-10 w-10'
+                            onClick={(event) => {
+                                dispatch({
+                                    type: "INCREMENT",
+                                })
+                            }}
+                        >
+                            +
+                        </button>
+                    </div>
+                </div>
+
+                <div className='flex flex-col w-full max-w-xs'>
+                    <label className='mb-3' htmlFor='feedback'>
+                        Feedback
+                    </label>
+                    <textarea
+                        name='feedback'
+                        id='feedback'
+                        cols='30'
+                        rows='4'
+                        className="border border-black rounded-sm"
+                        onBlur={(event) => {
+                            dispatch({
+                                type: "INPUT",
+                                paylode: { name: event.target.name, value: event.target.value }
+                            })
+                        }}
+                    ></textarea>
+                </div>
+
+                <div className='flex justify-between items-center w-full'>
+                    <div className='flex  w-full max-w-xs'>
+                        <input
+                            className='mr-3'
+                            type='checkbox'
+                            name='term'
+                            id='terms'
+                            onClick={() => {
+                                dispatch({
+                                    type: "TOGGOL",
+                                })
+                            }}
+                        />
+                        <label htmlFor='terms'>I agree to terms and conditions</label>
+                    </div>
+                    <button
+                        className=' px-4 py-3 bg-indigo-500 rounded-md font-semibold text-white text-lg disabled:bg-gray-500'
+                        type='submit'
+                        disabled={!state.term}
+                    >
+                        Submit
+                    </button>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default LongForm;
